4dbce97f6636ee3104d0981c4fd2eb4163873ea8
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2022-10-04 09:29:10 +0200

    fin ejercicio 1

--- ejercicio1-2/src/component/Counter.jsx
+++ ejercicio1-2/src/component/Counter.jsx
@@ -1,15 +1,21 @@
-import React, {  } from 'react';
+import React, { useEffect } from 'react';
 import { useCounter } from '../hook/useCounter';
 
 const Counter = () => {
 
     const{ count, handleDecrement, handleIncrement,handleReset, handleStep} = useCounter(0)
-   
+    
+    useEffect(() => {
+      if(count === 10 | count === -1){
+        alert('Has llegado al limite')
+      } 
+    }, [count])
+    
 
     return (
         <div>
             <h2>Contador</h2>
-            <p>{count}</p>
+            <p>{count  }</p>
             <button onClick={handleIncrement}>Incrementar 1</button>
             <button onClick={handleDecrement}>Decrementar 1</button>
             <button onClick={handleStep}>Incrementar 2</button>
